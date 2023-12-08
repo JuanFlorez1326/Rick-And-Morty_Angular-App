170bca78a692a6db6a6499238bfafd39a77d3d36
Juan Florez <jpatino@latinia.com>
2023-12-08 18:30:10 -0500

    NEW: Added filtering for character and location modules

--- src/app/characters/components/characters-list/characters-list.component.ts
+++ src/app/characters/components/characters-list/characters-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, SimpleChanges } from '@angular/core';
 import { Character } from '../../interfaces/characters.interface';
 
 @Component({
@@ -8,7 +8,31 @@ import { Character } from '../../interfaces/characters.interface';
 })
 export class CharactersListComponent {
 
-  @Input() characters!: Character[] | null;
+  @Input() characters!: any;
   @Input() isLoading!: boolean | null;
 
+  public leakedCharacters: any[] = [];
+  public allStatus: string[] = [];
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes) this.getAllStatus();
+    if ( changes['characters'] && this.characters ) {
+      this.leakedCharacters = changes['characters'].currentValue;
+    }
+  }
+
+  public filter( value: string ) {
+    if ( value === 'all' ) return this.leakedCharacters = this.characters;
+    this.leakedCharacters = this.characters.filter( (character: Character) => {
+      return character.status.toLowerCase().includes( value.toLowerCase() );
+    });
+  }
+
+  public getAllStatus() {
+    const status: string[] = [];
+    this.characters.forEach( (character: Character) => {
+      if ( !status.includes( character.status ) ) status.push( character.status );
+    });
+    this.allStatus = status.sort();
+  }
 }
--- src/app/location/components/location-list/location-list.component.ts
+++ src/app/location/components/location-list/location-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, SimpleChanges } from '@angular/core';
 import { Locations } from '../../interfaces/location.interface';
 
 @Component({
@@ -8,6 +8,31 @@ import { Locations } from '../../interfaces/location.interface';
 })
 export class LocationListComponent {
 
-  @Input() locations!: Locations[] | null;
+  @Input() locations!: any;
   @Input() isLoading!: boolean | null;
+
+  public leakedLocations: any[] = [];
+  public types: string[] = [];
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes) this.getAllTypes();
+    if ( changes['locations'] && this.locations ) {
+      this.leakedLocations = changes['locations'].currentValue;
+    }
+  }
+
+  public filter( value: string ) {
+    if ( value === 'all' ) return this.leakedLocations = this.locations;
+    this.leakedLocations = this.locations.filter( (character: Locations) => {
+      return character.type.toLowerCase().includes( value.toLowerCase() );
+    });
+  }
+
+  public getAllTypes() {
+    const types: string[] = [];
+    this.locations.forEach( (character: Locations) => {
+      if ( !types.includes( character.type ) ) types.push( character.type );
+    });
+    this.types = types.sort();
+  }
 }
